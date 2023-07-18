1d0d71e9871e2b0087690c0393e21f6029c9711c
SoonHyeon <qjsdl12@naver.com>
2023-07-18 09:23:06 +0900

    안녕!

---
+++ 0718_1/0718_1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0718_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("안녕!");
+        }
+    }
+}
