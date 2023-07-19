256b7ae20bdc68c5eeeb12186d0f30693c9dfa80
SoonHyeon <qjsdl12@naver.com>
2023-07-19 09:06:46 +0900

    with 은희 씨

---
+++ review1/review1/review.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace review1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] tree = new int[100]; //tree는 100개가 있다
+            int[] apart = new int[5] { 1, 2, 3, 4, 5, }; //아파트는 1호~5호가 있다
+            int[,] plate = new int[3, 3]; //2차원
+            int[,,] cube = new int[3, 3, 3];// 3차원 도형
+
+        }
+    }
+}
+
+/// 0 , 1 , 2 
